092ec34fac41a37ac719fc8e9e19b5040f8c92cf
Natan Barbosa <natanbarbosa15@gmail.com>
2021-07-28 20:07:26 -0300

    feat: Code push and Staging.

--- mobile/src/App.tsx
+++ mobile/src/App.tsx
@@ -3,6 +3,7 @@ import React, { useEffect } from 'react';
 import { View, StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import SplashScreen from 'react-native-splash-screen';
+import codePush from 'react-native-code-push';
 
 import AppProvider from './hooks';
 
@@ -29,4 +30,6 @@ const App: React.FC = () => {
   );
 };
 
-export default App;
+export default codePush({
+  checkFrequency: codePush.CheckFrequency.ON_APP_RESUME,
+})(App);
